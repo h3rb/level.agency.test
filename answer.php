10c15f92a4ac3470f90024104ce488e21028d09a
h3rb <hgilliland@gmail.com>
2017-03-30 12:24:21 -0400

    is_number

--- answer.php
+++ answer.php
@@ -15,6 +15,12 @@ function decipher( $a ) {
  return $a;
 }
 
+function is_number( $a ) {
+ $b=strsplit($a);
+ foreach ( $b as $c ) switch ( $c ) case '0':case '1':case '2':case '3': case '4':case '5':case '6':case '7':case '8':case '9':break;default:return FALSE;break;}
+ return TRUE;
+}
+
 class rover_console {
  
  var $w, $h, $rov_x, $rov_y, $rov_dir, $pseudo, $travelled; 
